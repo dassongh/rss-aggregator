2a65c9defbf2a5efacbbe36009ad8d40b9a315f1
Alexander S <oleksandr.slv@gmail.com>
2025-02-16 22:28:45 +0200

    feat: init server

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/dassongh/rss-aggregator
 
 go 1.23.5
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi/v5 v5.2.1
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world ")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is missing")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port: ", portString)
 }
